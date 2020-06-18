aea335414a8bae3f81e540090b7775939cdfc711
carson <carson@leaddyno.com>
2020-06-18 14:06:33 -0700

    include js

--- www/.eleventy.js
+++ www/.eleventy.js
@@ -3,5 +3,6 @@ const pluginSass = require("eleventy-plugin-sass");
 module.exports = function(config) {
     config.addPassthroughCopy("css");
     config.addPassthroughCopy("img");
+    config.addPassthroughCopy("js");
     config.addPlugin(pluginSass, {});
 }
