4ded0e05a6c826649ef884ccfd822ddb57d53e6b
carson <carson@leaddyno.com>
2020-06-18 19:51:30 -0700

    include tests

--- www/.eleventy.js
+++ www/.eleventy.js
@@ -4,5 +4,6 @@ module.exports = function(config) {
     config.addPassthroughCopy("css");
     config.addPassthroughCopy("img");
     config.addPassthroughCopy("js");
+    config.addPassthroughCopy("test");
     config.addPlugin(pluginSass, {});
 }
