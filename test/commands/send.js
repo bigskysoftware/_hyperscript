dbf6a9ff49a5ec709f840c58ee521ce970e3da46
carson <carson@leaddyno.com>
2020-06-30 10:32:47 -0700

    support colons in event names for htmx

--- test/commands/send.js
+++ test/commands/send.js
@@ -23,5 +23,36 @@ describe("the send command", function() {
         bar.innerHTML.should.equal("42");
     })
 
+    it("can send events with dots", function(){
+        var div = make("<div _='on click send foo.bar to #bar'></div>");
+        var bar = make("<div id='bar' _='on foo.bar add .foo-sent'></div>");
+        bar.classList.contains("foo-sent").should.equal(false);
+        div.click();
+        bar.classList.contains("foo-sent").should.equal(true);
+    })
+
+    it("can send events with dots with args", function(){
+        var div = make("<div _='on click send foo.bar(x:42) to #bar'></div>");
+        var bar = make("<div id='bar' _='on foo.bar put event.detail.x into my.innerHTML'></div>");
+        bar.classList.contains("foo-sent").should.equal(false);
+        div.click();
+        bar.innerHTML.should.equal("42");
+    })
+
+    it("can send events with colons", function(){
+        var div = make("<div _='on click send foo:bar to #bar'></div>");
+        var bar = make("<div id='bar' _='on foo:bar add .foo-sent'></div>");
+        bar.classList.contains("foo-sent").should.equal(false);
+        div.click();
+        bar.classList.contains("foo-sent").should.equal(true);
+    })
+
+    it("can send events with colons with args", function(){
+        var div = make("<div _='on click send foo:bar(x:42) to #bar'></div>");
+        var bar = make("<div id='bar' _='on foo:bar put event.detail.x into my.innerHTML'></div>");
+        bar.classList.contains("foo-sent").should.equal(false);
+        div.click();
+        bar.innerHTML.should.equal("42");
+    })
 
 });
--- test/features/on.js
+++ test/features/on.js
@@ -15,6 +15,14 @@ describe("the on feature", function() {
         div.classList.contains("called").should.equal(true);
     })
 
+    it("can respond to events with colons in names", function(){
+        var bar = make("<div _='on click send example:event to #d1'></div>");
+        var div = make("<div id='d1' _='on example:event add .called'></div>");
+        div.classList.contains("called").should.equal(false);
+        bar.click();
+        div.classList.contains("called").should.equal(true);
+    })
+
     it("can respond to events on other elements", function(){
         var bar = make("<div id='bar'></div>");
         var div = make("<div _='on click from #bar add .clicked'></div>");
