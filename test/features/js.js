d58fbaf862ae56b91a9e717b25a7cff43614fd97
Deniz Akşimşek <deniz@denizaksimsek.com>
2021-02-18 23:24:36 +0300

    fix syntax errors in tests

--- test/features/js.js
+++ test/features/js.js
@@ -12,30 +12,28 @@ describe('The (top-level) js feature', function () {
     					  "  js " +
     					  "    window.testSuccess = true " +
     					  "  end ");
-    	assert.equals(window.testSuccess, true);
+    	assert.equal(window.testSuccess, true);
     	delete window.testSuccess;
     })
 
     it('can expose globals', function() {
-    	window.testSuccess = false
     	var script = make("<script type=text/hyperscript>" +
     					  "  js " +
-    					  "    return { foo: 'test succeeded' }; "
+    					  "    return { foo: 'test succeeded' }; " +
     					  "  end ");
-    	assertEquals(window.foo, 'test succeeded')
+    	assert.equal(window.foo, 'test succeeded')
     	delete window.foo
     })
 
     it('can expose functions', function() {
-    	window.testSuccess = false
     	var script = make("<script type=text/hyperscript>" +
     					  "  js " +
     					  "    function foo() { " +
     					  "      return 'test succeeded'; " +
     					  "    } " +
-    					  "    return { foo: foo }; "
+    					  "    return { foo: foo }; " +
     					  "  end ");
-    	assertEquals(window.foo(), 'test succeeded')
+    	assert.equal(window.foo(), 'test succeeded')
     	delete window.foo
     })
 })
