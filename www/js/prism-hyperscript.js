7b61ecca2d66b6f019fdc18a4b0797214a8c45c5
Deniz Akşimşek <deniz@denizaksimsek.com>
2021-03-30 10:24:22 +0000

    update prism language dfn

--- www/js/prism-hyperscript.js
+++ www/js/prism-hyperscript.js
@@ -1,24 +1,33 @@
 
 (Prism => {
-	Prism.languages.hyperscript = {
+	Prism.languages.hyperscript = {};
+	Object.assign(Prism.languages.hyperscript, {
 		'comment': /\-\-.*/,
-		'punctuation': /[(){}[\],:]/,
+		'punctuation': /[(){}[\],:;\?&]/,
 		'keyword': {
-			pattern: /\b(?:on|def|js|worker|eventsource|socket|init|add|async|call|get|fetch|hide|measure|if|js|log|put|remove|repeat forever|repeat for|repeat in|repeat while|repeat until event|repeat until|repeat|return|send|settle|set|show|take|throw|toggle|transition|trigger|wait for|wait|tell|go|then|end|while|until|for|in|from|to|with|over)\b/g,
+			pattern: /\b(?:on|def|js|worker|eventsource|socket|init|behavior|install|add|async|call|get|fetch|hide|measure|if|js|log|put|remove|repeat\sforever|repeat\sfor|repeat\sin|repeat\swhile|repeat\suntil\sevent|repeat until|repeat|return|send|settle|set|show|take|throw|toggle\sbetween|toggle|transition|trigger|wait for|wait|tell|go|then|end|while|until|for|in|from|to|with|over|is an|is a|am|as|and|or|no|closest|the|of|first|last|on|seconds|milliseconds|s|ms)\b/g,
 			inside: {
-				'bold': /\b(?:on|def|js|worker|eventsource|socket|init|add|async|call|get|fetch|hide|measure|if|js|log|put|remove|repeat forever|repeat for|repeat in|repeat while|repeat until event|repeat until|repeat|return|send|set|settle|show|take|throw|toggle|transition|trigger|wait for|wait|tell|go|end)\b/g
+				'marker': {
+					pattern: /\b(?:on|def|js|worker|eventsource|socket|init|behavior|install|add|async|call|get|fetch|hide|measure|if|js|log|put|remove|repeat\sforever|repeat\sfor|repeat\sin|repeat\swhile|repeat\suntil\sevent|repeat\suntil|repeat|return|send|set|settle|show|take|throw|toggle\sbetween|toggle|transition|trigger|wait for|wait|tell|go|end|for)\b/g,
+					alias: 'bold',
+				}
 			}
 		},
-		'operator': /\+|-|\/|\*|<\b|>|<=|>=|\b(?:is|am|as|and|or|no|closest|the|of|first|last|on|is\sa|is\san)\b/,
+		'operator': {
+			pattern: /\+|\s-\s|\/|\*|\\|<\s|>|<=|>=|==|===|!=|!==|=|\.\.|(\w)\.|%|\||!|$/,
+			lookbehind: true,
+		},
 		'builtin': /\b(?:I|me|my|it|its|result|event|target|detail)\b/,
-		'function': /[-A-Za-z0-9]+(?=\()/i,
+		'function': /[A-Za-z0-9]+(?=\()/,
 		'class-ref': {
-			pattern: /\s\.[-\w\d_\$]+\b/,
-			alias: 'selector'
+			pattern: /\s\.[\-\w\d_\$]+/,
+			alias: 'selector',
+			greedy: true
 		},
 		'id-ref': {
-			pattern: /\s#[-\w\d_\$]\b/,
-			alias: 'selector'
+			pattern: /#[\-\w\d_\$]+/,
+			alias: 'selector',
+			greedy: true
 		},
 		'selector': /<[^\s].*\/>/,
 		'boolean': /\b(?:true|false|null)\b/,
@@ -26,12 +35,33 @@
 			pattern: /("[^\n]*[^\\]?"|'[^\n]*[^\\]?')/,
 			greedy: true,
 		},
+		'number': /(\d+\.?|\d*\.\d+)(s|ms)?/,
 		'hs-template-literal': {
-			pattern: /`[^\n`]*`/,
+			pattern: /`[^\n`]*`/, // ` //
 			greedy: true,
 			inside: {
-				'string': /`.*\$\{|`.*`|\}*\$\{|\}.*`/
+				'template-punctuation': {
+					pattern: /^`|`$/,
+					alias: 'string'
+				},
+				'interpolation': {
+					pattern: /((?:^|[^\\])(?:\\{2})*)\${(?:[^{}]|{(?:[^{}]|{[^}]*})*})+}/,
+					lookbehind: true,
+					inside: {
+						'interpolation-punctuation': {
+							pattern: /^\${|}$/,
+							alias: 'punctuation'
+						},
+						rest: Prism.languages.javascript
+					},
+				},
+				'string': /[\s\S]+/,
 			}
 		}
+	})
+
+	if (Prism.languages.markup) {
+		Prism.languages.markup.tag.addInlined('script', 'hyperscript')
+		Prism.languages.markup.tag.addAttribute('_', 'hyperscript')
 	}
 })(Prism)
