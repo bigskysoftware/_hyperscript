3cffddd51494c5b55ab8d1dd34abed8d5023aedf
carson <carson@leaddyno.com>
2021-06-13 06:02:53 -0600

    add halt command to reference

--- www/js/lib/hdb.js
+++ www/js/lib/hdb.js
@@ -1,6 +1,5 @@
 (function () {
-
-	var globalScope = typeof self !== 'undefined' ? self : typeof global !== 'undefined' ? global : this
+	var globalScope = typeof self !== "undefined" ? self : typeof global !== "undefined" ? global : this;
 
 	function HDB(ctx, runtime, breakpoint) {
 		this.ctx = ctx;
@@ -19,55 +18,64 @@
 			op: function (ctx) {
 				var hdb = new HDB(ctx, runtime, this);
 				window.hdb = hdb;
-				try{return hdb.break(ctx);}catch(e){console.error(e, e.stack)}
-			}
+				try {
+					return hdb.break(ctx);
+				} catch (e) {
+					console.error(e, e.stack);
+				}
+			},
 		};
-
-	})
+	});
 
 	var markerStyle = "color: #006aff; font-family: sans-serif;";
 	var logTypeStyle = "font-style: italic; font-family: sans-serif;";
 	var sourceStyle = "color: greenyellow; background: black;";
 	var symbolStyle = "font-style: italic;";
 	var infoStyle = "font-weight: bold; font-family: sans-serif;";
-	var headingStyle = "font-weight: bold; font-size: 1.4em; border: 1px solid #006aff; padding: .2em; display: block; width: max-content; max-width: 100%;"
-	
-	HDB.prototype.break = function(ctx) {
+	var headingStyle =
+		"font-weight: bold; font-size: 1.4em; border: 1px solid #006aff; padding: .2em; display: block; width: max-content; max-width: 100%;";
+
+	HDB.prototype.break = function (ctx) {
 		var self = this;
 		console.log("%c=== HDB///_hyperscript/debugger ===", headingStyle);
 		self.ui();
 		return new Promise(function (resolve, reject) {
-			self.bus.addEventListener("continue", function () {
-				if (self.ctx !== ctx) {
-					// Context switch
-					for (var attr in ctx) {
-						delete ctx[attr];
+			self.bus.addEventListener(
+				"continue",
+				function () {
+					if (self.ctx !== ctx) {
+						// Context switch
+						for (var attr in ctx) {
+							delete ctx[attr];
+						}
+						Object.assign(ctx, self.ctx);
 					}
-					Object.assign(ctx, self.ctx);
-				}
-				delete window.hdb;
-				resolve(self.runtime.findNext(self.cmd, self.ctx));
-			}, { once: true });
-		})
-	}
+					delete window.hdb;
+					resolve(self.runtime.findNext(self.cmd, self.ctx));
+				},
+				{ once: true }
+			);
+		});
+	};
 
-	HDB.prototype.continueExec = function() {
+	HDB.prototype.continueExec = function () {
 		this.bus.dispatchEvent(new Event("continue"));
-	}
+	};
 
-	HDB.prototype.stepOver = function() {
+	HDB.prototype.stepOver = function () {
 		var self = this;
 		if (!self.cmd) return self.continueExec();
-		var result = self.cmd && self.cmd.type === 'breakpointCommand' ?
-			self.runtime.findNext(self.cmd, self.ctx) :	
-			self.runtime.unifiedEval(self.cmd, self.ctx);
+		var result =
+			self.cmd && self.cmd.type === "breakpointCommand"
+				? self.runtime.findNext(self.cmd, self.ctx)
+				: self.runtime.unifiedEval(self.cmd, self.ctx);
 		if (result.type === "implicitReturn") return self.stepOut();
 		if (result && result.then instanceof Function) {
 			return result.then(function (next) {
 				self.cmd = next;
 				self.bus.dispatchEvent(new Event("step"));
 				self.logCommand();
-			})
+			});
 		} else if (result.halt_flag) {
 			this.bus.dispatchEvent(new Event("continue"));
 		} else {
@@ -75,9 +83,9 @@
 			self.bus.dispatchEvent(new Event("step"));
 			this.logCommand();
 		}
-	}
+	};
 
-	HDB.prototype.stepOut = function() {
+	HDB.prototype.stepOut = function () {
 		var self = this;
 		if (!self.ctx.meta.caller) return self.continueExec();
 		var callingCmd = self.ctx.meta.callingCommand;
@@ -85,28 +93,34 @@
 		self.ctx = self.ctx.meta.caller;
 		console.log(
 			"%c[hdb] %cstepping out into %c" + self.ctx.meta.feature.displayName,
-			markerStyle, logTypeStyle, symbolStyle)
+			markerStyle,
+			logTypeStyle,
+			symbolStyle
+		);
 		if (self.ctx.me instanceof Element && self.ctx.me !== oldMe) {
-			console.log("%c[hdb] %cme: ", markerStyle, logTypeStyle, self.ctx.me)
+			console.log("%c[hdb] %cme: ", markerStyle, logTypeStyle, self.ctx.me);
 		}
 		self.cmd = self.runtime.findNext(callingCmd, self.ctx);
 		self.cmd = self.runtime.findNext(self.cmd, self.ctx);
 		self.logCommand();
-		self.bus.dispatchEvent(new Event('step'))
-	}
+		self.bus.dispatchEvent(new Event("step"));
+	};
 
-	HDB.prototype.logCommand = function() {
+	HDB.prototype.logCommand = function () {
 		var hasSource = this.cmd.sourceFor instanceof Function;
-		var cmdSource = hasSource ? this.cmd.sourceFor() : '-- '+this.cmd.type;
+		var cmdSource = hasSource ? this.cmd.sourceFor() : "-- " + this.cmd.type;
 		console.log(
-			"%c[hdb] " +        "%ccurrent command: " + "%c"+cmdSource,
-			markerStyle,        logTypeStyle,           hasSource ? sourceStyle : infoStyle);
-	}
+			"%c[hdb] " + "%ccurrent command: " + "%c" + cmdSource,
+			markerStyle,
+			logTypeStyle,
+			hasSource ? sourceStyle : infoStyle
+		);
+	};
 
 	var ui = `
 <div class="hdb" _="
 	on load or step from hdb.bus send update to me
-	on continue from hdb.bus remove #hyperscript-hdb-ui-wrapper-">
+	on continue from hdb.bus log 'done' then remove me.getRootNode().host">
 
 	<script type="text/hyperscript">
 	def highlightDebugCode
@@ -180,11 +194,11 @@
 	</section>
 
 	<section class="sec-code">
-		<h3 _="on update from hdbUI
+		<h3 _="on update from .hdb
 			put 'Debugging <code>'+hdb.cmd.parent.displayName+'</code>' into me"></h3>
 
 		<div class="code-container">
-			<pre class="code" _="on update from hdbUI
+			<pre class="code" _="on update from .hdb
 			                          if hdb.cmd.programSource
 				                        put highlightDebugCode() into my.innerHTML
 				                        scrollIntoView({ block: 'nearest' }) the
@@ -197,7 +211,7 @@
 		<h3>Context</h3>
 
 		<dl class="context" _="
-			on update from hdbUI
+			on update from .hdb
 			set my.innerHTML to ''
 			repeat for var in Object.keys(hdb.ctx) if var != 'meta'
 				get '<dt>'+var+'<dd>'+prettyPrint(hdb.ctx[var])
@@ -360,16 +374,13 @@
 	}
 	</style>
 </div>
-`
+`;
 	HDB.prototype.ui = function () {
-		var node = document.createElement('div');
-		var shadow = node.attachShadow({ mode: 'open' });
-		node.style = 'all: initial';
-		node.id = 'hyperscript-hdb-ui-wrapper-';
+		var node = document.createElement("div");
+		var shadow = node.attachShadow({ mode: "open" });
+		node.style = "all: initial";
 		shadow.innerHTML = ui;
 		document.body.appendChild(node);
-		window.hdbUI = shadow.querySelector('.hdb');
-		_hyperscript.processNode(hdbUI);
-	}
-})()
-
+		_hyperscript.processNode(shadow.querySelector(".hdb"));
+	};
+})();
---
+++ www/test/0.1.0/test/commands/fetch.js
@@ -0,0 +1,134 @@
+describe("the fetch command", function () {
+	beforeEach(function () {
+		clearWorkArea();
+		sinon.stub(window, "fetch");
+	});
+	afterEach(function () {
+		sinon.restore();
+		clearWorkArea();
+	});
+
+	it("can do a simple fetch", function (done) {
+		window.fetch.returns(
+			Promise.resolve(
+				new window.Response("yay", {
+					status: 200,
+					headers: { "Content-type": "text/html" },
+				})
+			)
+		);
+		var div = make("<div _='on click fetch \"/test\" then put it into my.innerHTML'></div>");
+		div.click();
+		setTimeout(function () {
+			div.innerHTML.should.equal("yay");
+			done();
+		}, 50);
+	});
+
+	it("can do a simple fetch w/ a naked URL", function (done) {
+		window.fetch.returns(
+			Promise.resolve(
+				new window.Response("yay", {
+					status: 200,
+					headers: { "Content-type": "text/html" },
+				})
+			)
+		);
+		var div = make("<div _='on click fetch /test then put it into my.innerHTML'></div>");
+		div.click();
+		setTimeout(function () {
+			div.innerHTML.should.equal("yay");
+			done();
+		}, 50);
+	});
+
+	it("can do a simple fetch w/ html", function (done) {
+		window.fetch.returns(
+			Promise.resolve(
+				new window.Response("<br>", {
+					status: 200,
+					headers: { "Content-type": "text/html" },
+				})
+			)
+		);
+		var div = make(
+			"<div _='on click fetch /test as html then log it then put it into my.innerHTML put its childElementCount into my @data-count'></div>"
+		);
+		div.click();
+		setTimeout(function () {
+			div.innerHTML.should.equal("[object DocumentFragment]");
+			div.dataset.count.should.equal("1");
+			done();
+		}, 50);
+	});
+
+	it("can do a simple fetch w/ json", function (done) {
+		window.fetch.returns(
+			Promise.resolve(
+				new window.Response('{"foo":1}', {
+					status: 200,
+					headers: { "Content-type": "application/json" },
+				})
+			)
+		);
+		var div = make(
+			"<div _='on click fetch /test as json then get result as JSON then put it into my.innerHTML'></div>"
+		);
+		div.click();
+		setTimeout(function () {
+			div.innerHTML.should.equal('{"foo":1}');
+			done();
+		}, 50);
+	});
+
+	it("can do a simple fetch with a response object", function (done) {
+		window.fetch.returns(
+			Promise.resolve(
+				new window.Response('{"foo":1}', {
+					status: 200,
+					headers: { "Content-type": "application/json" },
+				})
+			)
+		);
+		var div = make("<div _='on click fetch /test as response then if its.ok put \"yep\" into my.innerHTML'></div>");
+		div.click();
+		setTimeout(function () {
+			div.innerHTML.should.equal("yep");
+			done();
+		}, 50);
+	});
+
+	it("can do a simple fetch w/ a custom conversion", function (done) {
+		window.fetch.returns(
+			Promise.resolve(
+				new window.Response("1.2000", {
+					status: 200,
+					headers: { "Content-type": "text/plain" },
+				})
+			)
+		);
+		var div = make("<div _='on click fetch /test as Number then put it into my.innerHTML'></div>");
+		div.click();
+		setTimeout(function () {
+			div.innerHTML.should.equal("1.2");
+			done();
+		}, 50);
+	});
+
+	it("can do a simple post", function (done) {
+		window.fetch.returns(
+			Promise.resolve(
+				new window.Response("yay", {
+					status: 200,
+					headers: { "Content-type": "text/html" },
+				})
+			)
+		);
+		var div = make("<div _='on click fetch /test {method:\"POST\"} then put it into my.innerHTML'></div>");
+		div.click();
+		setTimeout(function () {
+			div.innerHTML.should.equal("yay");
+			done();
+		}, 50);
+	});
+});
