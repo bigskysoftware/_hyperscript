0c464a5d364107ec4507211e690147db0bbe325c
carson <carson@leaddyno.com>
2021-06-14 08:11:10 -0600

    remove bad comment

--- test/features/on.js
+++ test/features/on.js
@@ -22,7 +22,6 @@ describe("the on feature", function () {
 		div.classList.contains("called").should.equal(true);
 	});
 
-	TODO enable strings in event names
 	it("can respond to events with minus in names", function(){
 	    var bar = make("<div _='on click send \"a-b\" to #d1'></div>");
 	    var div = make("<div id='d1' _='on \"a-b\" add .called'></div>");
