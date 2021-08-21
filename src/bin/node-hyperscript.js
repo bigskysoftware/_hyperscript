88f1cc48ce248e4d1b6c3813aa99cbf334d83239
Deniz Akşimşek <deniz@denizaksimsek.com>
2021-08-21 23:18:24 +0300

    support running ._hs files on the command line

---
+++ src/bin/node-hyperscript.js
@@ -0,0 +1,12 @@
+
+const _hyperscript = require('../lib/core');
+const fs = require('fs/promises');
+
+global.require = require; // Allow importing modules from within hyperscript
+
+const argv = process.argv.slice(2);
+fs.readFile(argv[0], { encoding: 'utf-8' }).then(code => {
+    _hyperscript(code);
+}).catch(e => {
+    console.error("Cannot execute file: ", e);
+})
