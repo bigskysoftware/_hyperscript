88584c589925f01fbba0845bd332ae42020c9ffa
carson <carson@leaddyno.com>
2021-09-30 19:58:41 -0600

    0.8.2 release prep

--- dist/_hyperscript.js
+++ dist/_hyperscript.js
@@ -2829,6 +2829,7 @@
 
 		_parser.addIndirectExpression("asExpression", function (parser, runtime, tokens, root) {
 			if (!tokens.matchToken("as")) return;
+			tokens.matchToken("a") || tokens.matchToken("an");
 			var conversion = parser.requireElement("dotOrColonPath", tokens).evaluate(); // OK No promise
 			var propertyAccess = {
 				type: "asExpression",
@@ -4671,17 +4672,24 @@
 		});
 
 		_parser.addGrammarElement("pseudoCommand", function (parser, runtime, tokens) {
-			var expr = parser.requireElement("primaryExpression", tokens);
-			if (expr.type !== "functionCall" && expr.root.type !== "symbol") {
+
+			tokens.pushFollow("of");
+			try {
+				var expr = parser.requireElement("primaryExpression", tokens);
+			} finally {
+				tokens.popFollow();
+			}
+			if (expr.type !== "functionCall" && expr.root.type !== "symbol" && expr.root.root != null) {
 				parser.raiseParseError(tokens, "Implicit function calls must start with a simple function");
 			}
-			// optional "on", "with", or "to"
-			if (!tokens.matchAnyToken("to", "on", "with", "into", "from", "at") && parser.commandBoundary(tokens.currentToken())) {
-				var target = parser.requireElement("implicitMeTarget", tokens);
-			} else {
-				var target = parser.requireElement("expression", tokens);
-			}
+
 			var functionName = expr.root.name;
+
+			if (tokens.matchAnyToken("to", "on", "with", "into", "from", "at", "of")) {
+				var target = parser.requireElement("expression", tokens);
+			} else if (tokens.matchToken("me")) {
+				var target = parser.requireElement("implicitMeTarget", tokens);
+			}
 			var functionArgs = expr.argExressions;
 
 			/** @type {GrammarElement} */
@@ -4690,7 +4698,11 @@
 				expr: expr,
 				args: [target, functionArgs],
 				op: function (context, target, args) {
-					var func = target[functionName];
+					if (target) {
+						var func = target[functionName];
+					} else {
+						var func = runtime.resolveSymbol(functionName, context);
+					}
 					if (func.hyperfunc) {
 						args.push(context);
 					}
@@ -5140,7 +5152,8 @@
 			var type = "text";
 			var conversion;
 			if (tokens.matchToken("as")) {
-				if (tokens.matchToken("json")) {
+				tokens.matchToken("a") || tokens.matchToken("an");
+				if (tokens.matchToken("json") || tokens.matchToken("Object")) {
 					type = "json";
 				} else if (tokens.matchToken("response")) {
 					type = "response";
---
+++ www/posts/2021-10-02-hyperscript-0.8.2-is-released.md
@@ -0,0 +1,18 @@
+---
+layout: layout.njk
+tags: post
+title: hyperscript 0.8.2 has been released!
+date: 2021-10-02
+---
+
+## hyperscript 0.8.2 Release
+
+We are pleased to present the
+[0.8.2 release](https://unpkg.com/browse/hyperscript.org@0.8.2/)
+of hyperscript.
+
+### Changes
+
+#TODO
+
+Enjoy!
