7e3222f79f5790d4b30000ed4b2e0cd7b8919109
Deniz Akşimşek <deniz@denizaksimsek.com>
2021-11-27 14:35:27 +0300

    Remove node-sass dependency

--- www/.eleventy.js
+++ www/.eleventy.js
@@ -1,9 +1,7 @@
-const pluginSass = require("eleventy-plugin-sass");
 
 module.exports = function(config) {
     config.addPassthroughCopy("css");
     config.addPassthroughCopy("img");
     config.addPassthroughCopy("js");
     config.addPassthroughCopy("test");
-    config.addPlugin(pluginSass, {});
 }
