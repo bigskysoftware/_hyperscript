f188e58047addd002149e0fc676d4d84ed36a079
Carson Gross <carson.gross@firstup.io>
2022-01-07 18:22:36 -0700

    support `break` to compliment `continue`

--- test/commands/repeat.js
+++ test/commands/repeat.js
@@ -280,4 +280,20 @@ describe("the repeat command", function () {
 		d1.click();
 		d1.innerHTML.should.equal("success A. success B. success C. expected D. success A. success B. success C. expected D. ");
 	});
+
+	it("loop break works", function () {
+		var d1 = make(
+			`<div _="on click
+				repeat 2 times
+					for x in ['A', 'B', 'C', 'D']
+						if x is 'C'
+						  break
+						end
+						put x at end of me
+					end
+				end
+			"></div>`);
+		d1.click();
+		d1.innerHTML.should.equal("ABAB");
+	});
 });
