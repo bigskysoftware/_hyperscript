878a7569c2184810eb4e91a8127c323a760eeec1
Arthur Wallendorff <howdy@wallenart.dev>
2025-02-22 16:27:44 +0100

    chore(wip): add tests

--- test/commands/clear.js
+++ test/commands/clear.js
@@ -36,6 +36,23 @@ describe("the clear command", function () {
         div2.innerHTML.should.equal("")
 	});
 
+    it("can clear elements by class name", function () {
+		var div = make("<div _='on click clear .target'><p>foo</p><p class='target'>bar</p></div>");
+        div.innerHTML.should.equal("<p>foo</p><p class=\"target\">bar</p>")
+		div.click();
+        div.innerHTML.should.equal("<p>foo</p><p class=\"target\"></p>")
+    })
+
+    // FIXME:
+    it("can clear elements by class name in other elements", function () {
+		var div = make("<div _='on click clear .target in #that'></div>");
+		var div2 = make("<div id='that'><p>foo</p><p class='target'>bar</p></div>");
+        div2.innerHTML.should.equal("<p>foo</p><p class=\"target\">bar</p>")
+		div.click();
+        console.log(div2.innerHTML)
+        div2.innerHTML.should.equal("<p>foo</p><p class=\"target\"></p>")
+    })
+
 	it("can clear parent element", function () {
 		var div = make("<div id='p1'><button  id='b1' _='on click clear my.parentElement'></button></div> ");
 		var btn = byId("b1");
@@ -44,7 +61,7 @@ describe("the clear command", function () {
         div.childElementCount.should.equal(0)
 	});
 
-	it("can clear specific things", function () {
+	it("can clear specific things by tag", function () {
 		var div = make("<div><div id='d1' _='on click clear <p/>'><p>foo</p>bar</div><p>doh</p></div>");
 		var d1 = byId('d1');
 		div.innerHTML.includes("foo").should.equal(true);
@@ -55,4 +72,8 @@ describe("the clear command", function () {
 		div.innerHTML.includes("bar").should.equal(true);
 		div.innerHTML.includes("doh").should.equal(true);
 	});
+
+    it("can clear an element that has a clear() method", function () {
+        // TODO:
+    })
 });
