86ef936a5f5be5492ced858199e3947629cf864c
Carson Gross <carson@bigsky.software>
2026-03-24 20:19:40 -0600

    update platform scripts to use the new ESM setup (and add bun)

--- src/platform/node-hyperscript.js
+++ src/platform/node-hyperscript.js
@@ -1,57 +1,61 @@
 #!/usr/bin/env node
 
-const _hyperscript = require('./_hyperscript.js')
-const fs = require('fs');
-const path = require('path')
+import _hyperscript from '../_hyperscript.js';
+import { Feature } from '../parsetree/base.js';
+import fs from 'node:fs';
+import path from 'node:path';
+import { createRequire } from 'node:module';
 
-/**
- * File extension for _hyperscript files
- */
 const hsExt = '._hs';
 
-global.require = require; // Allow importing modules from within hyperscript
-
-/**
- * 
- * @param {String} modulePath
- */
 function run(modulePath) {
     modulePath = path.resolve(modulePath);
-    const args = { module: { dir: path.dirname(modulePath), id: modulePath } }
+    const args = { module: { dir: path.dirname(modulePath), id: modulePath } };
     return fs.promises.readFile(modulePath, { encoding: 'utf-8' })
         .then(code => _hyperscript.evaluate(code, {}, args))
-        .catch(e => console.error("Cannot execute file: ", e));
+        .catch(e => console.error("Cannot execute file:", e));
 }
 
-_hyperscript.addFeature('require', (parser, runtime, tokens) => {
-    if (!tokens.matchToken('require')) return;
-    /** @type {string} */
-    let id = parser.requireElement('nakedString', tokens)
-        // @ts-ignore
-        .evaluate({});
+class RequireFeature extends Feature {
+    static keyword = "require";
 
-    let name = id;
-    if (tokens.matchToken('as')) {
-        name = tokens.requireTokenType('IDENTIFIER').value;
-    } else {
-        name = path.basename(id)
-            .replace(/\.[^\.]*$/, '') // remove extension
+    constructor(id, name) {
+        super();
+        this.moduleId = id;
+        this.moduleName = name;
     }
 
-    return {
-        install(target, source, args) {
-            if (id.startsWith('./') || id.startsWith('../')) {
-                id = path.join(args.module.dir, id);
-            }
-
-            let mod;
-            if (id.endsWith(hsExt)) mod = run(id);
-            if (fs.existsSync(id + hsExt)) mod = run(id + hsExt);
-            else mod = require(id);
-            runtime.assignToNamespace(target, [], name, mod);
-            //console.log(id, name, mod.toString(), target.hyperscriptFeatures);
+    static parse(parser) {
+        if (!parser.matchToken("require")) return;
+        var id = parser.requireElement("nakedString").evaluate();
+        var name;
+        if (parser.matchToken("as")) {
+            name = parser.requireTokenType("IDENTIFIER").value;
+        } else {
+            name = path.basename(id).replace(/\.[^.]*$/, '');
         }
+        return new RequireFeature(id, name);
     }
-})
 
-run(process.argv[2])
+    install(target, source, args, runtime) {
+        var id = this.moduleId;
+        if (id.startsWith('./') || id.startsWith('../')) {
+            id = path.join(args.module.dir, id);
+        }
+
+        var mod;
+        if (id.endsWith(hsExt)) {
+            mod = run(id);
+        } else if (fs.existsSync(id + hsExt)) {
+            mod = run(id + hsExt);
+        } else {
+            var nodeRequire = createRequire(args.module.id);
+            mod = nodeRequire(id);
+        }
+        runtime.assignToNamespace(target, [], this.moduleName, mod);
+    }
+}
+
+_hyperscript.addFeature(RequireFeature.keyword, RequireFeature.parse.bind(RequireFeature));
+
+run(process.argv[2]);
